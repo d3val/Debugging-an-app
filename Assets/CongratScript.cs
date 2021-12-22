0bd9392c8a8eaec62934909e4476fa1676c33821
d3val <devalt5008@gmail.com>
2021-12-22 16:45:39 -0600

    errors fixed

--- Assets/CongratScript.cs
+++ Assets/CongratScript.cs
@@ -1,50 +1,58 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CongratScript : MonoBehaviour
 {
     public TextMesh Text;
     public ParticleSystem SparksParticles;
-    
+
     private List<string> TextToDisplay;
-    
+
     private float RotatingSpeed;
     private float TimeToNextText;
 
     private int CurrentText;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        TextToDisplay = new List<string>();
+
         TimeToNextText = 0.0f;
-        CurrentText = 0
-        
-        RotatingSpeed = 1.0;
+        CurrentText = 0;
+
+        RotatingSpeed = 30.0f;
 
         TextToDisplay.Add("Congratulation");
         TextToDisplay.Add("All Errors Fixed");
 
         Text.text = TextToDisplay[0];
-        
+
         SparksParticles.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float step = RotatingSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, step);
+
         TimeToNextText += Time.deltaTime;
 
         if (TimeToNextText > 1.5f)
         {
             TimeToNextText = 0.0f;
-            
+
             CurrentText++;
             if (CurrentText >= TextToDisplay.Count)
             {
                 CurrentText = 0;
+            }
 
 
             Text.text = TextToDisplay[CurrentText];
         }
     }
+
 }
